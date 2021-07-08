6fde644d60e81941bd70c830707ccb99a9aa5ed3
neb <nebb_bb@protonmail.com>
2021-07-08 15:52:50 -0400

    wip

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  parser: "babel-eslint",
+  parser: "@babel/eslint-parser",
   parserOptions: {
     sourceType: "module",
-    allowImportExportEverywhere: true
+    allowImportExportEverywhere: true,
   },
   extends: [
     "airbnb",
@@ -16,60 +16,69 @@ module.exports = {
     "plugin:promise/recommended",
     "plugin:prettier/recommended",
     "prettier",
-    "prettier/react"
   ],
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   rules: {
     camelcase: 0,
     curly: ["error", "all"],
+    "consistent-return": "error",
     "no-restricted-imports": [
       "error",
       {
         paths: [
           {
             name: "rebass",
-            message: "Please use rebass/styled-components instead."
-          }
-        ]
-      }
+            message: "Please use rebass/styled-components instead.",
+          },
+        ],
+      },
     ],
-    // 'prettier/prettier': 0,
+    "promise/always-return": 0,
     "react/boolean-prop-naming": [
       "error",
-      { rule: "^(is|has|will|(.*)On)[A-Z]([A-Za-z0-9]?)+" }
+      { rule: "^(is|has|will|(.*)On)[A-Z]([A-Za-z0-9]?)+" },
     ],
     "react/forbid-prop-types": 0,
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
     "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
     "react/jsx-props-no-spreading": 0,
-    "react/jsx-sort-props": "error",
+    "react/jsx-sort-props": ["error", { ignoreCase: true }],
     "react/no-array-index-key": 0,
     "react/no-did-mount-set-state": 0,
     "react/prefer-stateless-function": 0,
     "react/require-default-props": 0,
     "react/sort-prop-types": "error",
-    "react/state-in-constructor": ['error', 'never'],
-    'react/static-property-placement': ['error', 'static public field'],
+    "react/state-in-constructor": ["error", "never"],
+    "react/static-property-placement": ["error", "static public field"],
     "react/style-prop-object": 0,
     "import/no-extraneous-dependencies": 0,
     "import/prefer-default-export": 0,
     "import/order": [
-      "warn",
+      "error",
       {
-        groups: [
-          ["external", "builtin"],
-          ["internal", "index", "sibling", "parent"]
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
         ],
-        "newlines-between": "never"
-      }
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
     ],
     "jsdoc/match-description": [
       "warn",
-      { matchDescription: "^(.* - )([A-Z]|[`\\d_])([\\s\\S]*[.?!`])?$" }
+      { matchDescription: "^(.* - )([A-Z]|[`\\d_])([\\s\\S]*[.?!`])?$" },
     ],
     "jsx-a11y/anchor-is-valid": ["error", { components: [] }],
     "jsx-a11y/no-static-element-interactions": 0,
@@ -80,16 +89,16 @@ module.exports = {
       {
         components: ["Label"],
         required: {
-          every: ["id"]
+          every: ["id"],
         },
-        allowChildren: false
-      }
+        allowChildren: false,
+      },
     ],
     "jsx-a11y/label-has-associated-control": [
       2,
       {
-        depth: 1
-      }
+        depth: 1,
+      },
     ],
     "max-len": ["error", { code: 120, ignoreUrls: true }],
     "no-cond-assign": ["error", "except-parens"],
@@ -103,20 +112,39 @@ module.exports = {
       "error",
       {
         array: false,
-        object: true
-      }
+        object: true,
+      },
     ],
-    "prefer-promise-reject-errors": 0
+    "prefer-promise-reject-errors": 0,
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
   plugins: [
+    "compat",
     "import",
     "json",
+    "jsdoc",
     "markdown",
     "prettier",
     "promise",
-    "compat",
-    "jsdoc",
     "react",
-    "react-hooks"
-  ]
+    "react-hooks",
+    "unused-imports",
+  ],
+  settings: {
+    polyfills: ["Promise"],
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+      },
+    },
+  },
 };
